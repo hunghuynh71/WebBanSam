4fbfdd58c49e1464c41996bbf9d3fbc2c0f4fac4
hunghuynh71 <hunghuynh813@gmail.com>
2023-08-16 22:55:51 +0700

    CatTemplateTrangBanHang_TrangChu

---
+++ WebBanSam/WebBanSam/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBanSam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
